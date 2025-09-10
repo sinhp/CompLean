7ad3b4c3fb50e6748a5b9c070e3e988e5547f0dd
sinhp <sinahazratpour@gmail.com>
2025-09-11 01:26:44 +0200

    m

--- CompLean/PrimRec/Definitions/Prelim.lean
+++ CompLean/PrimRec/Definitions/Prelim.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2025 Sina Hazratpour. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Sina Hazratpour
+-/
+
 import Mathlib.Data.Nat.Notation
 import Mathlib.Data.Fin.Tuple.Basic
 
