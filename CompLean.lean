ce4efb7f9a7d3fcf00fa3b7753b3d724ac2ca3c4
primrec <primrec@users.noreply.github.com>
2025-09-13 00:08:45 +0200

    make `lake build` build entire project

--- CompLean.lean
+++ CompLean.lean
@@ -1,3 +1,7 @@
 -- This module serves as the root of the `CompLean` library.
 -- Import modules here that should be built as part of the library.
 import CompLean.Basic
+import CompLean.Recursive.Definitions.Ackermann
+import CompLean.Recursive.Definitions.Prelim
+import CompLean.Recursive.Definitions.PrimRec
+import CompLean.Recursive.Theorems.Prelim
